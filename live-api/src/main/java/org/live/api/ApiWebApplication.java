2e3e92740a179aab0d0929c5548bcafc23a79602
handsonalex <378274190@qq.com>
2023-09-07 14:07:59 +0800

    1、live-api和user-provider集群部署 2、live-api更新logback 3、live-api引入nacos配置中心

--- live-api/src/main/java/org/live/api/ApiWebApplication.java
+++ live-api/src/main/java/org/live/api/ApiWebApplication.java
@@ -1,5 +1,6 @@
 package org.live.api;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
